04bd2703c166099c20045cc140a8d684de43db00
dimascs73 <skus.dima@me.com>
2023-10-26 18:13:48 +0300

    modified:   Static_Class.cpp 	modified:   .vscode/settings.json 	modified:   permutation.cpp

--- Static_Class.cpp
+++ Static_Class.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
+#include <windows.h>
+#include <fstream>
 
 using namespace std;
 
-
-class MyClass
+class Spisok
 {
 private:
     
@@ -12,48 +13,94 @@ private:
     string FirstName;
     string MiddleName;
     int ID;
-    
+
+friend ostream& operator<<(ostream& os, Spisok& Man);
+friend istream& operator>>(istream& is, Spisok& Man);
 
 public:
 
     static int Count;
 
-    MyClass(string LastName, string FirstName, string MiddleName){
-        
+    int getID(){
+        return ID;     
+    }
+    string GetName(){
+        return LastName;
+        return FirstName;
+        return MiddleName;
+    }
+    void SetName(string LastName, string FirstName, string MiddleName){
+
         this -> LastName = LastName;
         this ->FirstName = FirstName;
         this ->MiddleName = MiddleName;
         Count++;
         ID = Count+2;
-    }
 
-    int getID(){
-        return ID;     
-    }
-
-    void Person(){
-        
-        cout<<"ID:0000"<<ID<<"  "<<LastName<<' '<<FirstName<<' '<<MiddleName<<' '<<endl;
     }
 
 };
 
-int MyClass::Count = 0;
+ostream& operator<<(ostream& os, Spisok& Man)
+{
+  
+  os <<Man.ID<<' '<<Man.LastName<<' '<<Man.FirstName<<' '<<Man.MiddleName<<'\n';
+  return os;
+}
+istream& operator>>(istream& is, Spisok& Man)
+{
+  
+  is >>Man.ID>>Man.LastName>>Man.FirstName>>Man.MiddleName;
+  return is;
+}
+int Spisok::Count = 0;
 
 int main() {
 setlocale (LC_ALL, "ru");
+char action;
 
-MyClass Ivanov ("Иванов", "Иван", "Иванович");
-MyClass Petrov ("Петров", "Петр", "Петрович");
+string path = "spisok.txt";
 
-//MyClass::Count;
+SetConsoleCP(1251);
 
-Ivanov.Person();
-Petrov.Person();
 
-cout<<MyClass::Count<<endl;
-
+fstream file;
+file.open(path, fstream::in | fstream::out | fstream::app);
+if (!file.is_open())
+{
+    cout<<"Ошибка открытия файла\n";
+}
+else{
+    cout<<"Файл открыт\n";
+    
+    cout<<"Нажмите 1 для записи в файл\n"
+        <<"Нажмите 2 для чтения из файла"<<endl;
+    action = getchar();
+    if (action == 49)
+    {
+        Spisok Man;
+        Man.SetName("Иванов", "Иван", "Иванович");
+        file<<Man;
+        Man.SetName("Петров", "Петр", "Петрович");
+        file<<Man;
+    }
+    if (action == 50)
+    {
+       while (true)
+       {
+        Spisok Man;
+        file>>Man;
+        if (file.eof())
+        {
+            break;
+        }
+        cout<<Man;
+       }    
+    }
+}
+file.close();
+SetConsoleCP(866); 
 system ("pause"); 
-    return 0;
+return 0;
 
 }
