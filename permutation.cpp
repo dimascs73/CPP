c84d17a295aed3246c5e551685facac1dd35d67e
dimascs73 <skus.dima@me.com>
2023-10-27 16:47:46 +0300

    modified:   permutation.cpp

--- permutation.cpp
+++ permutation.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 void mass_input(const int size, int* arg){
       for (int i = 0; i < size; i++) // Заполняем массив
         {
-            arg[i] = i+1;
+            arg[i] = i;
             cout<<arg[i]<<'\t';
        }
         cout<<endl;
@@ -15,38 +16,38 @@ void mass_input(const int size, int* arg){
 
 }
 
+void swapM (const int size, int* arg){
+//vector <int> arg [size];
+  for (int k = 0 ; k < size - 1 ; k++)
+  {
+      
+  for (int i = k-1; i < size -1; i++)
+  {
+    swap (arg[i], arg[i+1]);
+    cout<<arg[i]<<'\t';
+  }
+  cout<<endl;
+  }
+
+}
 
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    const int size = 4;
-    int arg[size];
+    int size = 4;
+    int* arg = new int[size];
 
-   mass_input(size, arg); //вводим массив
+    mass_input(size, arg); //вводим массив
       
       // делаем перестановки
 
-  
-    for (int k = size - 1; k > 0; k--)
-    {
+    swapM (size, arg);
+      
     
-    int tmp = 0;
-    for (int j = size; j > 0; j--)
-    {
-      
-      for (int i = 0 ; i < size -1 ; i++)
-      {
-        cout<<arg[i]<<'\t';
-      }
-      cout<< endl;
-      tmp = arg[j];
-      arg[j] = arg[j+1];
-      arg[j+1] = tmp;
-      
-    }
     cout<<"___________"<<endl;
-    }
-         
+    
+    delete[] arg;
+
      system ("pause");
   
    return 0;
