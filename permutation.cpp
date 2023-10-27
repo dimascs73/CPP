c817e2d0063732b41f426e3bdc0f47f030ce6b7c
dimascs73 <skus.dima@me.com>
2023-10-27 16:45:17 +0300

    modified:   .vscode/settings.json 	new file:   perestanovki.cpp 	modified:   permutation.cpp

--- permutation.cpp
+++ permutation.cpp
@@ -15,27 +15,39 @@ void mass_input(const int size, int* arg){
 
 }
 
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    const int size = 3;
+    const int size = 4;
     int arg[size];
 
    mass_input(size, arg); //вводим массив
       
       // делаем перестановки
-    int tmp = 0;
-    for (int j = 0; j < size -1 ; j++)
+
+  
+    for (int k = size - 1; k > 0; k--)
     {
+    
+    int tmp = 0;
+    for (int j = size; j > 0; j--)
+    {
+      
+      for (int i = 0 ; i < size -1 ; i++)
+      {
+        cout<<arg[i]<<'\t';
+      }
+      cout<< endl;
       tmp = arg[j];
       arg[j] = arg[j+1];
-      cout<<*arg<<endl;
+      arg[j+1] = tmp;
+      
     }
-    
-    
-           
-        
-    system ("pause");
+    cout<<"___________"<<endl;
+    }
+         
+     system ("pause");
   
    return 0;
 }
