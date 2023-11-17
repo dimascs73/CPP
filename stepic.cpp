21ef241f67386e225b0d17ad60e8e27092139294
dimascs73 <skus.dima@me.com>
2023-11-17 14:55:21 +0300

    modified:   stepic.cpp

--- stepic.cpp
+++ stepic.cpp
@@ -12,18 +12,15 @@ int main()
    double y_big = x + 7;
    double y2_small = x - 2;
 
-   if (x >= -7 && x <= 2 )
+   if ((x >= -7 && x <= 0) && (y >= 0 && y <=7) && (y <= y_big))
    {
-        if (y >= 0 && y <= y_big)
-        {
-            cout<<"Inside";
-        }
-        if (y >= y2_small && y<=0)
-        {
         cout<<"Inside";
-        }
-        
    }
-   else cout<<"Outside";
+    else if ((x >= 0 && x <= 2) && (y >= -2 && y <= 0) && (y >= y2_small))
+   {    
+        cout<<"Inside";
+   }
+   else {cout<<"Outside";}
+
     return 0;
 }
