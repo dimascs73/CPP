ef0060665c19af797d9c6c69c7121378a7cb9b5f
dimascs73 <skus.dima@me.com>
2023-11-22 17:49:23 +0300

    new file:   lesson_fun.cpp 	modified:   stepic.cpp

---
+++ lesson_fun.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void Fun (int &a, int &b)
+{
+    int i = 0;
+
+    if (a > b)
+    {
+        i = a - b;
+
+    }
+    else if (a < b)
+    {
+        i = b - a;
+    }
+    
+    cout<<"Sum is: "<<i<<'\n';
+    
+}
+
+void Fun (double &a, double &b)
+{
+    cout<<"Devide "<<a<<"/"<<b<<" = "<<a/b<<'\n';
+
+}
+
+
+void Fun (double &a, int &b)
+
+{
+    cout<<"Multiply "<<a<<"*"<<b<<" = "<<a*b<<'\n';
+
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    
+    int x = 5;
+    int y = 7;
+    double z = 5.0;
+    double t = 2.0;
+
+Fun (x , y);
+
+Fun (z , t);
+
+Fun (z, x);
+
+
+ /*    int arr [x] = {1, 2, 3, 4, 5};
+    int arr2 [x];
+
+    for(int i =0; i < x; i++)
+{
+    arr2[i] = arr[i];
+}
+
+   for(int i =0; i < x; i++)
+{
+    cout<<"Arr_2 "<<i<<":"<<arr2[i]<<"\t Arr"<<i<<":"<<arr[i]<<'\n';
+}    
+ */
+
+
+    return 0;
+
+}
--- stepic.cpp
+++ stepic.cpp
@@ -5,20 +5,45 @@ using namespace std;
 
 int main()
 {
-   double x, y;
-
-   cin>>x>>y;
-  
-  double y1 = x+6;
-  double y2 = -x+5;
-  double y3 = x-3;
-  double y4 = -x-7;
    
-   if (((x <= y1) && (x >= y4) && (x <= y2 )) || ((x >= y3) && (x <= y4) && (x >= y2 )))
-        {cout<<"Inside";}
+    double x = 0;
+    double y = 0;
     
+    cin>>x>>y;
     
-   else {cout<<"Outside";}
+    bool inside = x*x+y*y <= 9;
+    bool inside_1 = (x+3)*(x+3)+(y+3)*(y+3) < 9;
+    bool inside_2 = (x-3)*(x-3)+(y-3)*(y-3) < 9;
+    bool inside_3 = (x-3)*(x-3)+(y+3)*(y+3) < 9;
+    bool inside_4 = (x+3)*(x+3)+(y-3)*(y-3) < 9;
+    
+    if (inside && !inside_1 && !inside_2 && !inside_3 && !inside_4)
+    {
+       std::cout<<"Inside"<<'\n';
+     }
+       else std::cout<<"Outside"<<'\n';
 
-    return 0;
-}
+        return 0;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
