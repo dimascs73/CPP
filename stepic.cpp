a58017a8a433ce3d37c7eed41795a2a0f15a9247
dimascs73 <skus.dima@me.com>
2023-11-23 20:14:40 +0300

    modified:   stepic.cpp

--- stepic.cpp
+++ stepic.cpp
@@ -8,16 +8,13 @@ int main()
    
     double x = 0;
     double y = 0;
+    //double eps = 1e-9;
     
     cin>>x>>y;
     
-    bool inside = x*x+y*y <= 9;
-    bool inside_1 = (x+3)*(x+3)+(y+3)*(y+3) < 9;
-    bool inside_2 = (x-3)*(x-3)+(y-3)*(y-3) < 9;
-    bool inside_3 = (x-3)*(x-3)+(y+3)*(y+3) < 9;
-    bool inside_4 = (x+3)*(x+3)+(y-3)*(y-3) < 9;
+    bool inside = y <= -3.*x - 1;
     
-    if (inside && !inside_1 && !inside_2 && !inside_3 && !inside_4)
+    if (inside)
     {
        std::cout<<"Inside"<<'\n';
      }
