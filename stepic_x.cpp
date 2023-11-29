aaee89a543767d9881d0ef61805e45115025f82a
dimascs73 <skus.dima@me.com>
2023-11-29 15:40:45 +0300

    modified:   stepic_x.cpp

--- stepic_x.cpp
+++ stepic_x.cpp
@@ -1,28 +1,21 @@
 ﻿#include <iostream>
-#include <cmath>
 
-using namespace std;
 
-int main()
-{
-   
-    double x = 0;
-    double y = 0;
-    
-    cin>>x>>y;
-    
-    bool inside = x*x+y*y > 3*3;
-    bool outside = x*x+y*y <= 5*5;
-    bool line1 = (x >= 0 && y >= 0) ; 
-    bool line2 = (x <= 0 && y <= 0) ; 
-    
 
-    if ((inside && outside && line1) || 
-        (inside && outside && line2))
-      {
-        std::cout<<"Inside"<<'\n';
-      }
-      else std::cout<<"Outside"<<'\n';
- 
-        return 0;
+int main() {
+  
+  int a = 0;
+  int b = 0;
+  int sum = 0;
+  
+
+  std::cin>>a>>b;
+
+  for (int i = a; i < b+1; i++)
+  {
+     sum +=i;   
+  }
+  std::cout<<sum<<std::endl;
+  
+  return 0;
 }
