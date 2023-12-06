5c41917040fcfb252468cddbeeed16dce5680fbc
dimascs73 <skus.dima@me.com>
2023-12-06 17:28:11 +0300

    modified:   stepic.cpp

--- stepic.cpp
+++ stepic.cpp
@@ -1,7 +1,4 @@
-﻿﻿#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
+﻿#include <iostream>
 #include <cmath>
 
 
@@ -12,8 +9,7 @@ int main()
     
     int v [a][a];
     int fig_i = 0, fig_j = 0;
-    int c_i = 0, d_j = 0;
-    int x_i = 0, y_j = 0;
+    
   
     
     
@@ -28,52 +24,29 @@ int main()
     }
     std::cout<<std::endl;
     std::cout<<std::endl;
-    if (fig_i > fig_j) {
-        c_i = fig_i - fig_j;
-        x_i = fig_i + fig_j;
+    
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < a; j++)
+        {
+            if (abs (i-fig_i) == abs(j - fig_j)) { v[i][j]=2; }
+        }
         
     }
-    else {
-        d_j = fig_j - fig_i;
-        y_j = fig_i + fig_j;
-    }
     
-    v[c_i][d_j] = 2;
-    v[x_i][y_j] = 2;
-    
-    
-    for (int k = 0; k < a ; k++) {
-        for ( int p = 0; p < a; p++) {
-            if (c_i > d_j) {
-                while (c_i != a-1) {
-                    c_i++;
-                    d_j++;
-                    v[c_i][d_j] =2;
-                    x_i++;
-                    y_j--;
-                    v[x_i][y_j] = 2;
-                }
-            }
-            else if (c_i <= d_j){
-                while (d_j != a-1) {
-                    c_i++;
-                    d_j++;
-                    v[c_i][d_j] = 2;
-                    x_i++;
-                    y_j--;
-                    v[x_i][y_j] = 2;
-            
-            }
-            }
-                v[fig_i][fig_j] = 1;
-            
+    v[fig_i][fig_j] = 1;
+
+    for (int i = 0; i < a; i++, std::cout<<std::endl)
+    {
+        for (int j = 0; j < a; j++)
+        {
+            std::cout<<v[i][j]<<' ';
         }
     }
-        
-    for (int i = 0; i < a; i++){
-    for (int j =0;j<a; j++){
-        std::cout<<v[i][j]<<' ';
-    }std::cout<<std::endl;
-}
+                
+            
+  
+
     return 0;
 }
+
