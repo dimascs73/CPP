a7a00666130affb3f7dc94970a0fc6b2439b86ef
dimaSCS73 <stilet1973@yandex.ru>
2023-12-28 23:56:30 +0300

    modified:   stepic.cpp

--- stepic.cpp
+++ stepic.cpp
@@ -5,6 +5,8 @@
 struct Expression 
 {
     virtual double evaluate() const = 0;
+
+    virtual ~Expression() {}
 };
 
 struct Number : Expression
@@ -12,37 +14,69 @@ struct Number : Expression
     Number(double value)
         : value(value)
     {}
+    double evaluate() const override
+    {
+        return value;
+    }
+    ~Number(){}
 
 private:
     double value;
 };
 
-struct BinaryOperation : Expression
+struct BinaryOperation : public Expression
 {
     /*
       Здесь op это один из 4 символов: '+', '-', '*' или '/', соответствующих операциям,
       которые вам нужно реализовать.
      */
     BinaryOperation(Expression const * left, char op, Expression const * right)
-        : left(left), op(op), right(right)
+        : left(left), right(right), op(op)
+    {}
+    double evaluate() const override
     {
+        double result  = left->evaluate();
         if (op == '+')
         {
-            left+right
+            result += right->evaluate();
         }
-        
+        else if (op == '*')
+        {
+            result *= right->evaluate();
+        }
+        else if (op == '-')
+        {
+            result -= right->evaluate();
+        }
+        else if (op == '/' && right->evaluate() !=0)
+        {
+            result /= right->evaluate();
+        }
+        else std::cerr<<"Error";
+
+       return result; 
     }
 
+    ~BinaryOperation()
+     {
+        delete left;
+        delete right;
+    }      
 private:
-    Expression const * left;
-    Expression const * right;
+
+    Expression const* left;
+    Expression const* right;
     char op;
+    
 };
+
+
+
 
 int main(int argc, char const *argv[])
 {
     
-    Expression * sube = new BinaryOperation(new Number(4.5), '*', new Number(5));
+    Expression* sube = new BinaryOperation(new Number(4.5), '*', new Number(5));
 
     Expression * expr = new BinaryOperation(new Number(3), '+', sube);
 
