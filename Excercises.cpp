1c8dcad8fd5a381d0d04a51f67a022286d3713ef
dimaSCS73 <stilet1973@yandex.ru>
2024-01-08 14:53:52 +0300

    This code for excersice 	modified:   Excercises.cpp

--- Excercises.cpp
+++ Excercises.cpp
@@ -11,46 +11,36 @@ private:
 
 public:
 
-std::string get_FirsttName() const
-  {
-    return first_name;
-  }
-std::string get_LasttName() const
-  {
-    return last_name;
-  }
-
-int get_Age() const
-  {
-    return age;
-  }
-
-  /* template <typename T>
-  T get_Data(T a) const
-  {
-    return a;
-  } */
+  void Set_Data();
   
-
-  void Set_Data(std::string first_name, std::string last_name, int age)
-  {
-    this -> first_name = first_name;
-    this -> last_name = last_name;
-    this -> age = age;
-  }
-  void PrintData()
-  {
-    std::cout<<first_name<<"  "<<last_name<<"  "<<age<<'\n';
-  }
+  void PrintData();
 };
+
+
+void Children::Set_Data()
+{
+  std::cout<<"Enter First Name: ";
+  std::cin>>first_name;
+  std::cout<<"Enter Last Name: ";
+  std::cin>>last_name;
+  std::cout<<"Enter Age: ";
+  std::cin>>age;
+
+}
+
+void Children::PrintData()
+{
+ std::cout<<first_name<<"  "<<last_name<<"  "<<age<<'\n';
+}
 
 int main()
 {
   Children ch1;
   Children ch2;
 
-  ch1.Set_Data("Martin", "McFly", 26);
-  ch2.Set_Data("Dima", "Skus", 51);
+  ch1.Set_Data();
+
+  ch2.Set_Data();
 
   ch1.PrintData();
 
