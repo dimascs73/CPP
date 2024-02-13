00089011871bc7f876398ecab16fa47507b260c2
dimascs73 <skus.dima@me.com>
2024-02-13 16:20:49 +0300

    modified:   stepic.cpp

--- stepic.cpp
+++ stepic.cpp
@@ -2,97 +2,74 @@
 #include <cstddef> // size_t
 #include <cstring> // strlen, strcpy
 
-struct Expression 
+struct Number;
+struct BinaryOperation;
+
+struct Visitor {
+    virtual void visitNumber(Number const * number) = 0;
+    virtual void visitBinaryOperation(BinaryOperation const * operation) = 0;
+    virtual ~Visitor() { }
+};
+
+struct Expression
 {
     virtual double evaluate() const = 0;
-
-    virtual ~Expression() {}
+    virtual void visit(Visitor * vistitor) const = 0;
+    virtual ~Expression() { }
 };
 
 struct Number : Expression
 {
-    Number(double value)
-        : value(value)
-    {}
-    double evaluate() const override
-    {
-        return value;
-    }
-    ~Number(){}
+    Number(double value);
+    double evaluate() const;
+
+    double get_value() const { return value; }
+
+    void visit(Visitor * visitor) const { visitor->visitNumber(this); }
 
 private:
     double value;
 };
 
-struct BinaryOperation : public Expression
+struct BinaryOperation : Expression
 {
-    /*
-      Здесь op это один из 4 символов: '+', '-', '*' или '/', соответствующих операциям,
-      которые вам нужно реализовать.
-     */
-    BinaryOperation(Expression const * left, char op, Expression const * right)
-        : left(left), right(right), op(op)
-    {}
-    double evaluate() const override
-    {
-        double result  = left->evaluate();
-        if (op == '+')
-        {
-            result += right->evaluate();
-        }
-        else if (op == '*')
-        {
-            result *= right->evaluate();
-        }
-        else if (op == '-')
-        {
-            result -= right->evaluate();
-        }
-        else if (op == '/' && right->evaluate() !=0)
-        {
-            result /= right->evaluate();
-        }
-        else std::cerr<<"Error";
+    BinaryOperation(Expression const * left, char op, Expression const * right);
+    ~BinaryOperation();
+    double evaluate() const;
 
-       return result; 
-    }
+    Expression const * get_left() const { return left; }
+    Expression const * get_right() const { return right; }
+    char get_op() const { return op; }
 
-    ~BinaryOperation()
-     {
-        delete left;
-        delete right;
-    }      
+    void visit(Visitor * visitor) const { visitor->visitBinaryOperation(this); }
+
 private:
-
-    Expression const* left;
-    Expression const* right;
+    Expression const * left;
+    Expression const * right;
     char op;
-    
 };
 
-bool check_equals(Expression const *left, Expression const *right)
-{
-    int *l = (int*)left;
-    int *r = (int*)right;
-    
-    if (*l == 4 && *r == 4)
+
+#include <iostream>
+
+/* Этот класс вам нужно реализовать */
+struct PrintVisitor : Visitor {
+    void visitNumber(Number const * number)
     {
-        return true;
+        /* ... */
     }
-    else return false;
-}
+
+    void visitBinaryOperation(BinaryOperation const * bop)
+    {
+        /* ... */
+    }
+};
 
 
 int main(int argc, char const *argv[])
 {
     
-    Expression* sube = new BinaryOperation(new Number(4.5), '*', new Number(5));
-
-    Expression * expr = new BinaryOperation(new Number(3), '+', sube);
-
-    std::cout << expr->evaluate() << std::endl;
-
-    delete expr;
-
+    
+    
     return 0;
 }
