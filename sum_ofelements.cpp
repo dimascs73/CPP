97a5ab2784448adf2b2cc17cc67fb2c060deacb8
dimascs73 <skus.dima@me.com>
2024-11-20 10:27:27 +0300

    modified:   sum_ofelements.cpp

--- sum_ofelements.cpp
+++ sum_ofelements.cpp
@@ -1,71 +1,38 @@
 ﻿#include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
- class Solution {
+class Solution {
 public:
-    bool isPalindrome(int x) {
-    int reverse = 0;
-    int ch_num = x;
-    bool result = true;
-//    vector <int>Sc{};
 
-    
-    /* while (x > 0)
-    {
-        Sc.push_back(x%10);
-        x /=10;
-    } */
-    
-    if (x < 0)
-    {
-        result = false;
-        cout <<"false";
-        return result;
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> value_index;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            
+            auto it = value_index.find(target - nums[i]);
+            if (it != value_index.end())
+            {
+                return {it->second, i};
+            }
+            value_index[nums[i]] = i;
 
+        }
+        return{};
     }
-    else {
-    while (ch_num > 0)
-    {
-        reverse = reverse*10 + ch_num % 10;
-        ch_num /=10;
-    }
-
-
-    /* for (int i = 0, k =  Sc.size() - 1; i < Sc.size()/2 && k >= Sc.size()/2 ; ++i, --k){
-        
-    if (Sc[i] == Sc[k])
-    {
-    result = true;
-    continue;
-    }
-    else result = false;
-    break;
-             
-    };
- */
-    x = x - reverse;
-    if (x  == 0)
-    {
-    cout <<"true";
-    }
-    else cout <<"false";
-    result = false;
-    }
-    return result; 
-    }
-}; 
-
-
+};
 
 int main(){
 
-int num = 11322311;
+int number = 9;
+vector<int> k{2, 4, 5, 8};
 
 Solution S;
 
-S.isPalindrome(num);
+S.twoSum(k, number);
+
   
 return 0;
 
