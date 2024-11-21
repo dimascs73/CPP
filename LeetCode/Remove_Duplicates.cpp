c73f8fc62edd5dbfe92597bf14aede88a4592971
dimascs73 <skus.dima@me.com>
2024-11-21 16:20:07 +0300

    modified:   LeetCode/Remove_Duplicates.cpp

--- LeetCode/Remove_Duplicates.cpp
+++ LeetCode/Remove_Duplicates.cpp
@@ -1,48 +1,37 @@
 ﻿#include <iostream>
-#include <list>
 #include <vector>
-#include <array>
 #include <algorithm>
 
 using std::vector;
-using std::list;
+using std::unique;
 
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int size_array = nums.size();
-        list<int> temp;
-        for (int i = 0; i < nums.size(); ++i){
-            temp.push_back(nums[i]);
+    int j = 1;
+    for (int i = 1; i < nums.size(); ++i){
+        
+        if (nums[i - 1] != nums[j]){
+            nums[j] = nums[i];
+            ++j;
         }
-        temp.sort();
-        temp.unique();
-        size_array -= temp.size();
-        return size_array;       
-    }
+        }
+        return j; 
+    }  
+    
 };
 
 int main()
 {
-    vector<int> new_vec{1,1,2,3,3};
+    vector<int> new_vec{0,0,1,1,1,2,2,3,3,4};
     
-    //Solution vk;
 
-    //vk.removeDuplicates(new_vec);
-    int vector_size = new_vec.size();
 
-    list<int> temp;
-    for (int i= 0; i < new_vec.size(); ++i ){
-        temp.push_back(new_vec[i]);
-        }
-    temp.sort();
-    temp.unique();
-    vector_size -= temp.size();
+    Solution vk;
 
-    for(auto k: temp){
-            std::cout<<k<<" ";
-        }
-    std::cout<<"\n"<<vector_size;
+    vk.removeDuplicates(new_vec);
+
+
 
     return 0;
 }
