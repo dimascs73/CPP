c16db259bd732a2de9bbbff4cd6c211118c34224
dimascs73 <skus.dima@me.com>
2024-11-28 17:47:07 +0300

    new file:   LeetCode/findIndex.cpp 	modified:   LeetCode/test.cpp

---
+++ LeetCode/findIndex.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <cmath>
+#include <vector>
+#include <ranges>
+#include <string>
+#include <algorithm>
+#include <windows.h>
+
+using std::vector;
+using std::string;
+
+class Solution {
+public:
+    int strStr(string S, string a) {
+        auto result = std::search (S.begin(), S.end(), a.begin(), a.end());
+    if ((result - S.begin()) >= S.size()){
+    return -1;}
+    else 
+    return (result - S.begin());
+    }   
+};
+
+int main(int argc, char const *argv[])
+{
+    string S{"sadbutsad"};
+    string a{"sad"};
+
+    Solution A;
+
+    std::cout<<A.strStr(S, a);
+
+    return 0;
+}
--- LeetCode/test.cpp
+++ LeetCode/test.cpp
@@ -10,48 +10,31 @@ using std::vector;
 using std::string;
 namespace rn = std::ranges;
 namespace vu = std::views;
-/*
-S[1][0], S[2][0], S[n][0]
-*/
+
 int main()
 {
-   vector<string> S{"flower", "kloor", "flood", "flower",};
-   string a{S[0]};
+   string S{"sadbutsad"};
+   string a{"mad"};
    //vector<string>::iterator it; 
 
-   string res{};
+   
 
-   for (int i = 0; i < a.size();){
-     for (int k = 1; k < S.size();){
-        
-        if (S[k][i] == a[i] && k == S.size() - 1 ){
-            res.push_back(a[i]);
-            ++i;
-            k = 1;
-        }
-        else if (S[k][i] == a[i] && k < S.size() ){
-            ++k;
-            continue;}
-        else if (S[k][i] != a[i]){break;}
-        
 
-     }
-    break; 
-   }
-    if (res == ""){
-        std::cout<<"";
+    auto result = std::search (S.begin(), S.end(), a.begin(), a.end()); result != S.end();
+    std::cout<<std::endl;
+    if ((result - S.begin()) >= S.size()){
+    std::cout<<-1;}
+    else {
+    std::cout<<std::endl;
+    std::cout<<result - S.begin();
     }
-    else {std::cout<<res<<std::endl;}
-
-    auto ru = S | vu::filter([&a](string s){ return s.contains(a);});
     
-    system("cls");
     
-    for (auto n: ru){
+    /* for (auto n: ru){
 
         std::cout<<n<<" ";
-    }
-std::cout<<S[0][3];
+    } */
+
    return 0;
 }
 
