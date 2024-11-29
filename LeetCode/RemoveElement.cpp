e88b1f709de008d5d543cc1716c6b978e1a6ed55
dimascs73 <skus.dima@me.com>
2024-11-29 15:21:45 +0300

    new file:   LeetCode/RemoveElement.cpp 	modified:   LeetCode/test.cpp

---
+++ LeetCode/RemoveElement.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+#include <vector>
+#include <ranges>
+
+using std::vector;
+namespace rn = std::ranges;
+namespace vu = std::views;
+
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        if (nums.size() == 1 && nums[0] == val)
+        {
+            return 0;
+        }
+        else if (nums.size() == 1 && nums[0] != val)
+        {
+            return 1;
+        }
+        auto res = nums | vu::filter([val] (int n) {return n != val;});
+                 
+        int k = 0;
+        for (int a : res){
+        ++k;     
+        }
+        if (k == 0){return 0;}
+        
+        int tmp = 0;
+        int i = 0;
+        int j = (nums.size() - 1);
+        while ( i <= j )
+        {
+            if (nums[i] == val && nums[j] != val){
+                tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+                ++i;
+                --j;
+            }
+            else if (nums[i] == val && nums[j] == val){
+                tmp = nums[i];
+                nums[i] = nums[i+1];
+                nums[i+1] = tmp;
+            }
+            else if (nums[i] != val && nums[j] == val){
+                --j;
+            }
+             else if (nums[i] != val ){
+                 ++i;
+            }
+            else {
+                ++i;
+                --j;}
+
+        }
+
+    return i;  
+    }
+};
+
+int main()
+{
+    vector<int> c{2,2,3};
+    int x = 2;
+
+    
+    Solution M;
+
+    M.removeElement(c, x);
+
+    for(auto& n: c){
+        std::cout<<n<<" ";
+    }
+
+    return 0;
+}
--- LeetCode/test.cpp
+++ LeetCode/test.cpp
@@ -11,30 +11,33 @@ using std::string;
 namespace rn = std::ranges;
 namespace vu = std::views;
 
+
+class Solution {
+public:
+    bool isValid(string s) {
+      bool n = 0;
+    if (s.size()%2 != 0){return n;}
+    for (int i = 0, j = (s.size()-1); i < s.size()/2, j >=s.size()/2; ++i, --j){
+        if ((s[j] - s[i]) == 2 || (s[j] - s[i]) == 1){n = 1; }
+        else n = false;} 
+
+    return n;
+    }
+};
+
+//если первые ( то последние ), 
+
 int main()
 {
-   string S{"sadbutsad"};
-   string a{"mad"};
-   //vector<string>::iterator it; 
+   string S = ("(){}}{");
 
    
 
+   
+   Solution x;
 
-    auto result = std::search (S.begin(), S.end(), a.begin(), a.end()); result != S.end();
-    std::cout<<std::endl;
-    if ((result - S.begin()) >= S.size()){
-    std::cout<<-1;}
-    else {
-    std::cout<<std::endl;
-    std::cout<<result - S.begin();
-    }
-    
-    
-    /* for (auto n: ru){
-
-        std::cout<<n<<" ";
-    } */
-
+   std::cout<<x.isValid(S);
+ 
    return 0;
 }
 
